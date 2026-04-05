a5365bee3eb04a9d8afb3ef52389bb308242ad96
YogevKr <yogev@sawmills.ai>
2026-04-05 18:19:55 +0300

    chore: release draftsx v0.1.1

--- Formula/draftsx.rb
+++ Formula/draftsx.rb
@@ -3,8 +3,9 @@ class Draftsx < Formula
 
   desc "CLI and rebuildable local index for Drafts.app"
   homepage "https://github.com/YogevKr/draftsx"
-  url "https://github.com/YogevKr/draftsx/releases/download/v0.1.0/draftsx-0.1.0.tar.gz"
-  sha256 "6b1a0f4fa9e8ba0fa330b977c1f72dee2edb2fae4657415c4dfad59047432aff"
+  url "https://github.com/YogevKr/draftsx/releases/download/v0.1.1/draftsx-0.1.1.tar.gz"
+  sha256 "9d14cb053e73293180744bbfa5c50e702e0cf628d723cee9c00b82099ad30210"
+  license "MIT"
 
   depends_on "python@3.13"
 
