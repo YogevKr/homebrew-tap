9bc49e828d0d21728825b8cb151f0d76d3784bf7
YogevKr <yogev@sawmills.ai>
2026-05-05 15:55:05 +0300

    chore: bump zt to 0.2.6

--- Formula/zt.rb
+++ Formula/zt.rb
@@ -1,14 +1,24 @@
 class Zt < Formula
   desc "Remote Zellij session manager for humans and automation"
   homepage "https://github.com/YogevKr/zt"
-  url "https://github.com/YogevKr/zt/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "66e8fe2f15e1bc27b0417bc793aec5eab60c74ac2ad98368d75ce64f7bb23ff7"
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/YogevKr/zt/releases/download/v0.2.6/zt-v0.2.6-aarch64-apple-darwin.tar.gz"
+    sha256 "66643906201cbfe6a80b0f7ce313a1ec284f5730df5efba41253a011680e391b"
+  else
+    url "https://github.com/YogevKr/zt/archive/refs/tags/v0.2.6.tar.gz"
+    sha256 "cc684f5a81950702b5010e64759a3d64e67baa6e1a93272ce3f327ab4cafce7e"
+  end
+
   license "MIT"
 
-  depends_on "rust" => :build
+  depends_on "rust" => :build if OS.linux? || Hardware::CPU.intel?
 
   def install
-    system "cargo", "install", *std_cargo_args
+    if OS.mac? && Hardware::CPU.arm?
+      bin.install "zt"
+    else
+      system "cargo", "install", *std_cargo_args
+    end
     prefix.install "README.md", "LICENSE"
   end
 
