a738c0e48fb6af5b5361f4396c5fa57acd2dc8eb
YogevKr <yogev@sawmills.ai>
2026-05-05 16:10:36 +0300

    fix: install zt via python wrapper

--- Formula/zt.rb
+++ Formula/zt.rb
@@ -2,7 +2,7 @@ class Zt < Formula
   desc "Remote Zellij session manager for humans and automation"
   homepage "https://github.com/YogevKr/zt"
   url "https://github.com/YogevKr/zt/archive/refs/tags/v0.1.4.tar.gz"
-  version "0.2.7"
+  version "0.2.12"
   sha256 "9aa7642f9578aa096910081f6fab3b74a73eb4e1d3178eb3349f1517bfde6eeb"
   license "MIT"
 
@@ -10,11 +10,18 @@ class Zt < Formula
 
   def install
     python = Formula["python@3.14"].opt_bin/"python3.14"
-    inreplace "zt", "#!/usr/bin/env python3", "#!#{python}"
-    inreplace "zt", 'VERSION = "0.1.4"', 'VERSION = "0.2.7"'
+    inreplace "zt", 'VERSION = "0.1.4"', 'VERSION = "0.2.12"'
 
-    bin.install "zt"
+    libexec.install "zt" => "zt.py"
+    (bin/"zt").write <<~SH
+      exec "#{python}" "#{libexec}/zt.py" "$@"
+    SH
+    File.chmod 0755, bin/"zt"
     prefix.install "README.md", "LICENSE"
+  end
+
+  def post_install
+    File.chmod 0755, bin/"zt"
   end
 
   test do
